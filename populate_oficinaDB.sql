2dca0121dcaa19b04c48755a8bcde6be3d84c56b
zumpchiat <zumpchiat@gmail.com>
2025-10-06 20:58:23 -0300

    feat: add populate_oficinaDB

---
+++ populate_oficinaDB.sql
@@ -0,0 +1,7 @@
+use oficina;
+
+-- POPULATE TABLE CLIENTE DADOS FAKE
+insert into oficina.cliente (nome, sobrenome, cpf, endereco, telefone, email, dtUltimaVisita) values 
+	("Roni", "Souza", "12121234356", "Rua dos cajueiros, centro niteroi 25410114", "2198786654", "rsouza@bol.com.br", "2025-02-12"),
+    ("Alan", "Santos", "189562374145", "Avenida dos Bandeirantes, centro Rio de Janeiro 26523880", "2199784456", "Alan189@globo.com.br", "2024-03-04"),
+    ("Roberto", "Malaquias", "98745698751", "Loteamento dos Ribeiros, ostia Sergipe 66523774", "6687874514", "roismal@gmail.com", "2023-03-04");
