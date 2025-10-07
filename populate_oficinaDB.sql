88bf268d25c12e80424afe298b26aa9c84b4ad5d
zumpchiat <zumpchiat@gmail.com>
2025-10-06 21:00:12 -0300

    feat: update table veiculo

--- populate_oficinaDB.sql
+++ populate_oficinaDB.sql
@@ -5,3 +5,12 @@ insert into oficina.cliente (nome, sobrenome, cpf, endereco, telefone, email, dt
 	("Roni", "Souza", "12121234356", "Rua dos cajueiros, centro niteroi 25410114", "2198786654", "rsouza@bol.com.br", "2025-02-12"),
     ("Alan", "Santos", "189562374145", "Avenida dos Bandeirantes, centro Rio de Janeiro 26523880", "2199784456", "Alan189@globo.com.br", "2024-03-04"),
     ("Roberto", "Malaquias", "98745698751", "Loteamento dos Ribeiros, ostia Sergipe 66523774", "6687874514", "roismal@gmail.com", "2023-03-04");
+
+
+-- POPULATE TABLE VEICULO DADOS FAKE
+insert into oficina.veiculo (placa, marca, modelo, chassi, tipoCombustivel, numeroPortas, ano, id_client) values 
+	("kom6788", "FORD", "Fiesta", "4534gdfr345g", "Gasolina", 5, "2010", 1),
+	("kom8799", "FIAT", "PALIO", "55634gdfr345g", "Flex", 5, "2016", 1),
+	("qng9987", "FIAT", "Siena", "55634gdjkrtty", "Alcool", 5, "2014", 2),
+    ("qng5511", "FORD", "OPALA", "qwsdfg98ee", "Gasolina", 5, "2014", 2);
+
--- schema.sql
+++ schema.sql
@@ -15,4 +15,21 @@ create table oficina.cliente(
     constraint unique_email unique(email)
 );
 
+-- CREATE TABLE VEICULO
+create table oficina.veiculo(
+	id_veiculo INT auto_increment primary key,
+    placa varchar(10),
+    marca varchar(20),
+    modelo varchar(20),
+    chassi varchar(60),
+    tipoCombustivel enum("Gasolina", "Alcool", "Flex", "Eletrico", "Disel"),
+    numeroPortas char(2),
+    ano varchar(6),
+    id_client INT,
+    constraint fk_client foreign key(id_client) references oficina.cliente(id_client),
+    constraint unique_placa unique(placa),
+    constraint unique_chassi unique(chassi)
+);
+
+ALTER TABLE oficina.veiculo auto_increment=1;
 ALTER TABLE oficina.cliente auto_increment=1;
